cdab6d57569746bdaac71b50038073b33f303318
JoyTaribagshaw <joytaribagshaw@gmail.com>
2024-03-02 12:56:33 +0000

    update styling by rendering button only at the last slide

--- anu-student-handbook/src/App.jsx
+++ anu-student-handbook/src/App.jsx
@@ -4,7 +4,6 @@ import slides from './components/slide';
 import './App.css';
 
 function App() {
-  
   const [startX, setStartX] = useState(null);
   const [isSwiping, setIsSwiping] = useState(false);
 
@@ -46,30 +45,29 @@ function App() {
   const handlePrevSlide = () => {
     setCurrentSlideIndex((prevIndex) => (prevIndex - 1 + slides.length) % slides.length);
   };
-  
 
   const [currentSlideIndex, setCurrentSlideIndex] = useState(0);
 
-
   return (
-   
-      <div
-        className='max-w-[1400px] w-full m-auto py-16 px-4 bg-slate-50'
-        onTouchStart={handleTouchStart}
-        onTouchMove={handleTouchMove}
-        onTouchEnd={handleTouchEnd}
-      >
-        <div className='w-full duration-500 flex flex-col justify-center items-start'>
-          {/* Render Current Slide */}
-          <div className="slide-container mx-auto">
-            <h1>{slides[currentSlideIndex].slide}</h1>
-          </div>
-        </div>
-        <div className='text-2xl cursor-pointer'>
-          <RxDotfilled />
+    <div
+      className='max-w-[1400px] w-full m-auto py-16 px-4 bg-slate-50'
+      onTouchStart={handleTouchStart}
+      onTouchMove={handleTouchMove}
+      onTouchEnd={handleTouchEnd}
+    >
+      <div className='w-full duration-500 flex flex-col justify-center items-start'>
+        {/* Render Current Slide */}
+        <div className="slide-container mx-auto">
+          <h1>{slides[currentSlideIndex].slide}</h1>
         </div>
       </div>
-    
+      <div className='text-2xl cursor-pointer'>
+        <RxDotfilled />
+      </div>
+      {currentSlideIndex === slides.length - 1 && (
+        <button className="bg-yellow-400 p-2 mt-5 rounded-full w-64 mx-auto text-xl">Getting Started</button>
+      )}
+    </div>
   );
 }
 
--- anu-student-handbook/src/components/slide.jsx
+++ anu-student-handbook/src/components/slide.jsx
@@ -29,7 +29,6 @@ const slides = [
         <div className="flex flex-col text-left">
           <span>No hustle to find <br/> updated info on campus 3<br/></span>
           <span style={style}>Get all the <br/>info you need <br/> in one place dtt</span>
-          <button className="bg-yellow-400 p-2 mt-5 rounded-full w-64 mx-auto text-xl">Getting Started</button>
         </div>
       ),
     },
