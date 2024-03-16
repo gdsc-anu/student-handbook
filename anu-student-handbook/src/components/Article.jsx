eb833f5ff00cd864e2a3f12c6589476f8adc9c4e
JoyTaribagshaw <joytaribagshaw@gmail.com>
2024-03-16 17:00:18 +0000

    style navbar and homepage dashboard

--- anu-student-handbook/src/components/Article.jsx
+++ anu-student-handbook/src/components/Article.jsx
@@ -1,14 +1,25 @@
-const bgImg = {
-    backgroundImage: 'url(https://www.figma.com/file/gjzrE5bfOhNoYWNSej5Ell/image/95c55ad69a3d6d4296e5e535bf741cf1e56c1916)',
-    backgroundRepeat: 'no-repeat',
-    backgroundPosition: 'center',
-    opacity: '.1',
-}
-
 export default function Article() {
     return (
-        <div className="h-full" style={bgImg}>
-            <h1>Article</h1>
+        <div className="h-full relative flex flex-col justify-between">
+            <div className="absolute inset-0" style={{
+                backgroundImage: 'url(https://www.figma.com/file/gjzrE5bfOhNoYWNSej5Ell/image/95c55ad69a3d6d4296e5e535bf741cf1e56c1916)',
+                backgroundRepeat: 'no-repeat',
+                backgroundPosition: 'center',
+                opacity: '.1',
+                zIndex: '-1',
+            }}></div>
+            <div>
+                <h1>Article</h1>
+            </div>
+            <div>
+                <div className="flex justify-around">
+                    <button className="bg-gray-300 py-1 px-6 rounded-2xl">Back</button>
+                    <button className="bg-gray-300 py-1 px-6 rounded-2xl">Next</button>
+                </div>
+                <div className="flex justify-center">
+                    <p className="bg-gray-200 py-1 px-4 rounded-2xl my-4">Made by GDSC-ANU 23/24</p>
+                </div>
+            </div>
         </div>
     )
 }
--- anu-student-handbook/src/components/Nav.jsx
+++ anu-student-handbook/src/components/Nav.jsx
@@ -1,17 +1,23 @@
-import {faBars} from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars, faSearch } from '@fortawesome/free-solid-svg-icons';
 
 export default function Nav() {
     return(
-        <div className="flex">
-            <FontAwesomeIcon icon={faBars} />
-            <input 
-                type="text" 
-                placeholder="Search" 
-            />
+        <div className="flex bg-gray-100 py-3 justify-around items-center">
+            <FontAwesomeIcon icon={faBars} size="2x" color="#C92A2A" />
+            <div className="relative">
+                <input 
+                    className="bg-gray-300 py-1 px-4 rounded-2xl w-96 pl-10"
+                    type="text" 
+                    placeholder="Search" 
+                />
+                <FontAwesomeIcon 
+                    icon={faSearch} 
+                    className="absolute top-0 left-3 mt-2 text-gray-500"
+                />
+            </div>
             <img 
-                src="../images/vector.png" 
+                src="src/images/icons8-chatbot-32.png" 
                 alt="vector"
             />
         </div>
