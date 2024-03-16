37a933c761cb340e0345350ccf795e942630b60a
JoyTaribagshaw <joytaribagshaw@gmail.com>
2024-03-16 15:22:04 +0000

    add opacity to background logo

--- anu-student-handbook/src/components/Article.jsx
+++ anu-student-handbook/src/components/Article.jsx
@@ -2,7 +2,7 @@ const bgImg = {
     backgroundImage: 'url(https://www.figma.com/file/gjzrE5bfOhNoYWNSej5Ell/image/95c55ad69a3d6d4296e5e535bf741cf1e56c1916)',
     backgroundRepeat: 'no-repeat',
     backgroundPosition: 'center',
-    backdropFilter: 'blur(5px)',
+    opacity: '.1',
 }
 
 export default function Article() {
